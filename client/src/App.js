84a83f9396c830160b1a20327c2d53967345d03c
MarcRipollFabrega <rip.marcf@gmail.com>
2025-03-21 11:29:38 +0100

    inserta noves dades a la BBDD MYSQL

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import {useState} from "react"
+import {useState} from "react";
+import Axios from "axios";
+
+
 
 function App() {
 
@@ -9,8 +12,16 @@ const [pais,setPais] =useState("");
 const [cargo,setCargo] =useState("");
 const [anios,setAnios] =useState(0);
 
-const mostrarDatos = ()=>{
-  alert(nombre);
+const add = ()=>{
+  Axios.post("http://localhost:3001/create",{
+    nombre:nombre,
+    edad:edad,
+    pais:pais,
+    cargo:cargo,
+    anios:anios
+  }). then(()=>{
+    alert("Empleado registrado")
+  });
 }
 
   return (
@@ -21,7 +32,7 @@ const mostrarDatos = ()=>{
   <label>País: <input onChange={(event)=>{ setPais(event.target.value); }} type="text"/></label>
   <label>Cargo: <input onChange={(event)=>{ setCargo(event.target.value); }} type="text"/></label>
   <label>Años: <input onChange={(event)=>{ setAnios(event.target.value); }} type="number"/></label>
-  <button onClick={mostrarDatos}>Registrar</button>
+  <button onClick={add}>Registrar</button>
 </div>
     </div>
   );
